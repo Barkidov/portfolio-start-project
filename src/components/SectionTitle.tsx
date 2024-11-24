41260e5e5ed4f9c4509f8cd4ec9136c5bad3c5e5
Barkidov <zpliskin@inbox.ru>
2024-11-24 21:51:29 +0300

    code rafactoring

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { theme } from "../styles/Theme";
 
 type SectionTitlePropsTyre = {
   title?: string;
@@ -14,4 +15,8 @@ export const SectionTitle = styled.h2<SectionTitlePropsTyre>`
   text-align: ${(props) => props.textAlign || "undefined"};
   font-size: ${(props) => props.fontSize || "2rem"};
   font-weight: ${(props) => props.fontWeight || "500"};
+
+  @media ${theme.media.tablet} {
+    padding: 50px 0 50px 0
+  }
 `;
